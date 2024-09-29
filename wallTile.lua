f6f329628cdc93f4b256f8a4804dd16d06c7f8bc
R. A. Pratama <rapratama2005@gmail.com>
2024-09-28 21:35:30 -0500

    fixed ball.velocity changes on collision

--- wallTile.lua
+++ wallTile.lua
@@ -44,7 +44,7 @@ function WallTile:checkCollision(ball)
     -- local collisionSide = self.super.checkCollision(ball)
     local collisionSide
 
-    if (inBtwn(self.topLeft.x, ball.center.x, self.topLeft.x + world.tileSize.width/2)) then
+    if (inBtwn(self.topLeft.x, ball.center.x, self.topLeft.x + world.tileSize.width)) then
         if (isWithin(ball.center.y, self.topLeft.y, ball.trueSize.height/2)) then
             collisionSide =  "top"
         elseif (isWithin(ball.center.y, self.topLeft.y + world.tileSize.height, ball.trueSize.height/2)) then
@@ -52,7 +52,7 @@ function WallTile:checkCollision(ball)
         end
     end
 
-    if (inBtwn(self.topLeft.y, ball.center.y, self.topLeft.y + world.tileSize.width/2)) then
+    if (inBtwn(self.topLeft.y, ball.center.y, self.topLeft.y + world.tileSize.width)) then
         if (isWithin(ball.center.x, self.topLeft.x, ball.trueSize.width/2)) then
             collisionSide = "left"
         elseif (isWithin(ball.center.x, self.topLeft.x + world.tileSize.width, ball.trueSize.width/2)) then
@@ -63,13 +63,13 @@ function WallTile:checkCollision(ball)
     if (collisionSide == "false") then
         return false
     elseif (collisionSide == "top") then
-        ball.velocity.y = -10
+        ball.velocity.y = -math.abs(ball.velocity.y)*.9
     elseif (collisionSide == "bottom") then
-        ball.velocity.y = 10
+        ball.velocity.y = math.abs(ball.velocity.y)*.9
     elseif (collisionSide == "left") then
-        ball.velocity.x = -10
+        ball.velocity.x = -math.abs(ball.velocity.x)*.9
     elseif (collisionSide == "right") then
-        ball.velocity.x = 10
+        ball.velocity.x = math.abs(ball.velocity.x)*.9
     end
     return true
 end
