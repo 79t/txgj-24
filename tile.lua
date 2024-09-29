11e89299782932151b25844547e3e8893ccf94dd
suhas <hi@suhas.one>
2024-09-28 20:25:14 -0500

    asdf

--- tile.lua
+++ tile.lua
@@ -32,6 +32,14 @@ function Tile:new()
 ---@diagnostic disable-next-line: missing-return
 end
 
+local function inBtwn(low, mid, up)
+    return low<mid and mid<up
+end
+
+local function isWithin(point1, point2, minDiff)
+    return math.abs(point1 - point2) < minDiff
+end
+
 -- TODO 
 ---@param ball Ball
 function Tile:checkCollision(ball)
@@ -39,23 +47,24 @@ function Tile:checkCollision(ball)
         return "false"
     end
 
-    if (self.inBetween(self.topLeft.x, ball.center.x, self.topLeft.x + world.tileSize.width)) then
-        if (self.isWithin(ball.center.y, self.topLeft.y, ball.trueSize.height)) then
+    if (inBtwn(self.topLeft.x, ball.center.x, self.topLeft.x + world.tileSize.width)) then
+        if (isWithin(ball.center.y, self.topLeft.y, ball.trueSize.height)) then
             return "top"
-        elseif (self.isWithin(ball.center.y, self.topLeft.y + world.tileSize.height, ball.trueSize.height)) then
+        elseif (isWithin(ball.center.y, self.topLeft.y + world.tileSize.height, ball.trueSize.height)) then
             return "bottom"
         end
     end
 
-    if (self.inBetween(self.topLeft.y, ball.center.y, self.topLeft.y + world.tileSize.width)) then
-        if (self.isWithin(ball.center.x, self.topLeft.x, ball.trueSize.width)) then
+    if (inBtwn(self.topLeft.y, ball.center.y, self.topLeft.y + world.tileSize.width)) then
+        if (isWithin(ball.center.x, self.topLeft.x, ball.trueSize.width)) then
             return "left"
-        elseif (self.isWithin(ball.center.x, self.topLeft.x + world.tileSize.width, ball.trueSize.width)) then
+        elseif (inBtwn(ball.center.x, self.topLeft.x + world.tileSize.width, ball.trueSize.width)) then
             return "right"
         end
     end
     return "false"
 end
+
 
 function Tile:inBetween(lower, middle, upper)
     return lower < middle and middle < upper
