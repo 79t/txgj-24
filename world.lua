ed375058e5b6713f14753e77a8aaa348f7b87a2a
suhas <hi@suhas.one>
2024-09-29 13:25:32 -0500

    do 32x32

--- world.lua
+++ world.lua
@@ -7,7 +7,8 @@ world = {
     levelFile = love.filesystem.read("assets/level0_c.txt"),
     size = {x = 16, y = 16},
     tileSize = {
-        width = 37.5, height = 37.5 --height/size
+        -- width = 37.5, height = 37.5 --height/size
+        width = 18.75, height = 18.75
     },
     tileMap = {},
     startPos = {x = 0, y = 0}
